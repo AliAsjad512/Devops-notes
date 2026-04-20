b738c753efbf8dbb8f2e3ed00f249feedd0d17dd
Ali Abbas <aa0504503@gmail.com>
2026-04-20 10:21:13 -0400

    class DatabaseBackup

--- Automation/DatabaseBackup.py
+++ Automation/DatabaseBackup.py
@@ -6,3 +6,14 @@ import gzip
 import datetime
 import tempfile
 from cryptography.fernet import Fernet
+class DatabaseBackup:
+    def __init__(self, db_type, host, port, user, password, database, s3_bucket, s3_prefix='backups/'):
+        self.db_type = db_type
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.database = database
+        self.s3 = boto3.client('s3')
+        self.s3_bucket = s3_bucket
+        self.s3_prefix = s3_prefix.rstrip('/') + '/'
