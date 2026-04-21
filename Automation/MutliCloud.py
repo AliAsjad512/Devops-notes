94cd7611f04572c8759af6ce1d727afb74575547
Ali Abbas <aa0504503@gmail.com>
2026-04-21 09:30:38 -0400

    Secret Synchromizer

--- Automation/MutliCloud.py
+++ Automation/MutliCloud.py
@@ -5,3 +5,9 @@ from google.cloud import secretmanager
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 import base64
+class SecretSynchronizer:
+    def __init__(self, source_cloud, source_config, dest_cloud, dest_config):
+        self.source = self._connect(source_cloud, source_config)
+        self.dest = self._connect(dest_cloud, dest_config)
+        self.source_cloud = source_cloud
+        self.dest_cloud = dest_cloud
