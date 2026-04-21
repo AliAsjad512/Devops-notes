3d59db31b3f4f37f1f6246a1281c6a2f3021b56e
Ali Abbas <aa0504503@gmail.com>
2026-04-21 09:31:15 -0400

    connect

--- Automation/MutliCloud.py
+++ Automation/MutliCloud.py
@@ -11,3 +11,12 @@ class SecretSynchronizer:
         self.dest = self._connect(dest_cloud, dest_config)
         self.source_cloud = source_cloud
         self.dest_cloud = dest_cloud
+    def _connect(self, cloud, config):
+        if cloud == 'aws':
+            return AWSSecretManager(config['region'])
+        elif cloud == 'gcp':
+            return GCPSecretManager(config['project_id'])
+        elif cloud == 'azure':
+            return AzureKeyVault(config['vault_url'])
+        else:
+            raise ValueError(f"Unsupported cloud: {cloud}")
