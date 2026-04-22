06d5ff128218620af14cc2b4b3b53e1d1274f76b
Ali Abbas <aa0504503@gmail.com>
2026-04-22 09:29:02 -0400

    Class ConfigManager

--- Automation/MultienvornmentConfi.py
+++ Automation/MultienvornmentConfi.py
@@ -5,3 +5,9 @@ import os
 import copy
 from pathlib import Path
 from deepdiff import DeepDiff
+
+class ConfigManager:
+    def __init__(self, base_config_path, env_config_dir):
+        self.base_config_path = Path(base_config_path)
+        self.env_config_dir = Path(env_config_dir)
+        self.base = self._load_yaml(base_config_path)
