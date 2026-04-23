6d42f2b22ed66d15f57a95b637ac8056708769e6
Ali Abbas <aa0504503@gmail.com>
2026-04-23 10:26:20 -0400

    list images

--- Automation/DockerImageClearner.py
+++ Automation/DockerImageClearner.py
@@ -5,3 +5,19 @@ from collections import defaultdict
 class DockerImageCleaner:
     def __init__(self):
         self.client = docker.from_env()
+
+    def list_images(self):
+        images = self.client.images.list(all=True)
+        img_data = []
+        for img in images:
+            tags = img.tags
+            if not tags:
+                tags = ['<none>:<none>']
+            img_data.append({
+                'id': img.id[:12],
+                'tags': tags,
+                'size': img.attrs['Size'],
+                'created': datetime.datetime.fromtimestamp(img.attrs['Created']),
+                'used_by_containers': len(img.attrs.get('ContainerConfig', {}).get('Hostname', [])) > 0
+            })
+        return img_data
