559360f5579587dafded5d75a9c093149a57bd2c
Ali Abbas <aa0504503@gmail.com>
2026-04-23 10:26:52 -0400

    find unused images

--- Automation/DockerImageClearner.py
+++ Automation/DockerImageClearner.py
@@ -21,3 +21,13 @@ class DockerImageCleaner:
                 'used_by_containers': len(img.attrs.get('ContainerConfig', {}).get('Hostname', [])) > 0
             })
         return img_data
+    def find_unused_images(self, days_old=30):
+        cutoff = datetime.datetime.now() - datetime.timedelta(days=days_old)
+        images = self.list_images()
+        unused = []
+        for img in images:
+            if img['used_by_containers']:
+                continue
+            if img['created'] < cutoff and not any(t != '<none>:<none>' for t in img['tags']):
+                unused.append(img)
+        return unused
