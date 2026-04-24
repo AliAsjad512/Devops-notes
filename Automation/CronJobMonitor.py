3d87b82d16ab68c3d939abf28c201c0abcb21b5e
Ali Abbas <aa0504503@gmail.com>
2026-04-24 18:52:21 -0400

    cron Monitor class

--- Automation/CronJobMonitor.py
+++ Automation/CronJobMonitor.py
@@ -5,3 +5,7 @@ import datetime
 import smtplib
 from email.mime.text import MIMEText
 from pathlib import Path
+
+class CronMonitor:
+    def __init__(self, log_file='/var/log/cron.log'):
+        self.log_file = Path(log_file)
