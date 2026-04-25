a1b089b5b8febc2779683174b69bd19f44066b7b
Ali Abbas <aa0504503@gmail.com>
2026-04-25 11:11:06 -0400

    Release Note Generator

--- Automation/ReleaseNoteGenerator.py
+++ Automation/ReleaseNoteGenerator.py
@@ -2,3 +2,6 @@ import subprocess
 import argparse
 import re
 from datetime import datetime, timedelta
+class ReleaseNotesGenerator:
+    def __init__(self, repo_path='.'):
+        self.repo_path = repo_path
