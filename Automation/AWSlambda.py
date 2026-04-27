1c8c32ea957ece7caa048b4ad4025e3a285e5fd2
Ali Abbas <aa0504503@gmail.com>
2026-04-27 10:51:18 -0400

    Lambda Log Tailer

--- Automation/AWSlambda.py
+++ Automation/AWSlambda.py
@@ -4,3 +4,8 @@ import time
 import datetime
 import threading
 from collections import deque
+ 
+ class LambdaLogTailer:
+    def __init__(self, region='us-east-1'):
+        self.logs = boto3.client('logs', region_name=region)
+        self.tailers = {}
