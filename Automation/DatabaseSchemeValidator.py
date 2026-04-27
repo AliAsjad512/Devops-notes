d85fee5de3d0a7772a56062153c6f40f867932d1
Ali Abbas <aa0504503@gmail.com>
2026-04-26 20:59:33 -0400

    class Schema Validator

--- Automation/DatabaseSchemeValidator.py
+++ Automation/DatabaseSchemeValidator.py
@@ -3,3 +3,8 @@ import json
 import psycopg2
 import pymysql
 from deepdiff import DeepDiff
+ 
+ class SchemaValidator:
+    def __init__(self, db_type, host, port, user, password, database):
+        self.db_type = db_type
+        self.conn = self._connect(host, port, user, password, database)
