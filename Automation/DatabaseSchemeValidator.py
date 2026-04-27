49b7e84a4a00c0d615c1d88c81e4837cc97073f4
Ali Abbas <aa0504503@gmail.com>
2026-04-26 21:00:08 -0400

    connect

--- Automation/DatabaseSchemeValidator.py
+++ Automation/DatabaseSchemeValidator.py
@@ -8,3 +8,9 @@ from deepdiff import DeepDiff
     def __init__(self, db_type, host, port, user, password, database):
         self.db_type = db_type
         self.conn = self._connect(host, port, user, password, database)
+    def _connect(self, host, port, user, password, database):
+        if self.db_type == 'postgres':
+            return psycopg2.connect(host=host, port=port, user=user, password=password, database=database)
+        else:
+            return pymysql.connect(host=host, port=port, user=user, password=password, database=database)
+
