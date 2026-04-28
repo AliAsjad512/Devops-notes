9b5b39a44f374353d5359cbf15bfc1ad680278e9
Ali Abbas <aa0504503@gmail.com>
2026-04-28 07:14:10 -0400

    PrometheusRuleValidtor

--- Automation/PromethesuRulesValidator.py
+++ Automation/PromethesuRulesValidator.py
@@ -4,3 +4,7 @@ import argparse
 import requests
 import re
 from pathlib import Path
+
+class PrometheusRuleValidator:
+    def __init__(self, prometheus_url=None):
+        self.prometheus_url = prometheus_url
