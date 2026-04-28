67a67753bddea4945ff4a530666a057bd07ad3ad
Ali Abbas <aa0504503@gmail.com>
2026-04-28 07:14:39 -0400

    load rules

--- Automation/PromethesuRulesValidator.py
+++ Automation/PromethesuRulesValidator.py
@@ -8,3 +8,7 @@ from pathlib import Path
 class PrometheusRuleValidator:
     def __init__(self, prometheus_url=None):
         self.prometheus_url = prometheus_url
+
+    def load_rules(self, rule_file):
+        with open(rule_file, 'r') as f:
+            return yaml.safe_load(f)
