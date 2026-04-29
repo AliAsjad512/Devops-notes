9fae5aeb4151e97d554f2aefaa7b5bd8c7c4fcfc
Ali Abbas <aa0504503@gmail.com>
2026-04-29 11:26:46 -0400

    Class ServiceNow incident

--- Automation/ServicNowIncidentCreator.py
+++ Automation/ServicNowIncidentCreator.py
@@ -2,3 +2,9 @@ import argparse
 import requests
 import json
 from datetime import datetime
+
+class ServiceNowIncident:
+    def __init__(self, instance, username, password):
+        self.url = f'https://{instance}.service-now.com/api/now/table/incident'
+        self.auth = (username, password)
+        self.headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
