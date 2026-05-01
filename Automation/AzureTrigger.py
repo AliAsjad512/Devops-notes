9a482f94420f3cde69fbe4948999f82a284a501f
Ali Abbas <aa0504503@gmail.com>
2026-05-01 00:06:46 -0400

    complete the code for azure class

--- Automation/AzureTrigger.py
+++ Automation/AzureTrigger.py
@@ -4,3 +4,12 @@ import base64
 import json
 
 class AzureDevOpsPipeline:
+    def __init__(self, org, project, pat):
+        self.org = org
+        self.project = project
+        self.auth = base64.b64encode(f":{pat}".encode()).decode()
+        self.headers = {
+            'Authorization': f'Basic {self.auth}',
+            'Content-Type': 'application/json'
+        }
+        self.base_url = f'https://dev.azure.com/{org}/{project}'
