e11129362f772bc725c4e8848d491101b6580981
Ali Abbas <aa0504503@gmail.com>
2026-05-02 11:17:43 -0400

    SSL Renewe class

--- Automation/SSL.py
+++ Automation/SSL.py
@@ -4,3 +4,10 @@ import logging
 import sys
 import os
 from pathlib import Path
+
+class SSLRenewer:
+    def __init__(self, certbot_path='certbot'):
+        self.certbot = certbot_path
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        self.logger = logging.getLogger(__name__)
+
