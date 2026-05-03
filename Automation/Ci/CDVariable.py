9da699f119cb5cb9c7015f2690434f562b858188
Ali Abbas <aa0504503@gmail.com>
2026-05-03 14:52:54 -0400

    add reqguired

--- Automation/Ci/CDVariable.py
+++ Automation/Ci/CDVariable.py
@@ -7,4 +7,7 @@ class EnvValidator:
     def __init__(self, required_vars=None, patterns=None):
         self.required_vars = required_vars or []
         self.patterns = patterns or {}
+    def add_required(self, var_name):
+        self.required_vars.append(var_name)
+
 
