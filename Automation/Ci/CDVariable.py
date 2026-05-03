225c673b97f72c0c1d1ca901f8ecf6a380f2a736
Ali Abbas <aa0504503@gmail.com>
2026-05-03 14:49:40 -0400

    EnvValidator

--- Automation/Ci/CDVariable.py
+++ Automation/Ci/CDVariable.py
@@ -2,3 +2,9 @@ import os
 import sys
 import argparse
 import re
+
+class EnvValidator:
+    def __init__(self, required_vars=None, patterns=None):
+        self.required_vars = required_vars or []
+        self.patterns = patterns or {}
+
