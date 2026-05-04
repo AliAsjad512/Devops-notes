b8f86e01b7f82c7c0822c32eefebc31b3793fcf1
Ali Abbas <aa0504503@gmail.com>
2026-05-04 13:51:58 -0400

    get containers

--- Automation/DockerlogsContainer.py
+++ Automation/DockerlogsContainer.py
@@ -9,3 +9,10 @@ from concurrent.futures import ThreadPoolExecutor
 class DockerLogCollector:
     def __init__(self):
         self.client = docker.from_env()
+    def get_containers(self, label_filter=None, name_filter=None):
+        filters = {}
+        if label_filter:
+            filters['label'] = label_filter
+        if name_filter:
+            filters['name'] = name_filter
+        return self.client.containers.list(filters=filters)
