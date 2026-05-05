388b740bb1b4436799cd9d0f446f7b9a127acbfe
Ali Abbas <aa0504503@gmail.com>
2026-05-05 02:08:41 -0400

    EBS class Snapshot Scheduler

--- Automation/AWSSnapShot.py
+++ Automation/AWSSnapShot.py
@@ -2,3 +2,8 @@ import boto3
 import argparse
 import datetime
 from collections import defaultdict
+
+class EBSSnapshotScheduler:
+    def __init__(self, region='us-east-1'):
+        self.ec2 = boto3.client('ec2', region_name=region)
+        self.region = region
