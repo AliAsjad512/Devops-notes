74212260cee161e0f3172cd948e3bf6100b4a173
Ali Abbas <aa0504503@gmail.com>
2026-05-06 14:07:52 -0400

    lint

--- Automation/HelmchartTester.py
+++ Automation/HelmchartTester.py
@@ -9,3 +9,6 @@ import json
 class HelmTester:
     def __init__(self, chart_path):
         self.chart_path = chart_path
+    def lint(self):
+        result = subprocess.run(['helm', 'lint', self.chart_path], capture_output=True, text=True)
+        return result.returncode == 0, result.stdout + result.stderr
