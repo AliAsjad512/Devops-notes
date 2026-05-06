5f00d6e0963447f9bcf082c2e3eabafa540e97c0
Ali Abbas <aa0504503@gmail.com>
2026-05-06 14:08:22 -0400

    template

--- Automation/HelmchartTester.py
+++ Automation/HelmchartTester.py
@@ -12,3 +12,14 @@ class HelmTester:
     def lint(self):
         result = subprocess.run(['helm', 'lint', self.chart_path], capture_output=True, text=True)
         return result.returncode == 0, result.stdout + result.stderr
+    def template(self, values_files=None, set_values=None, release_name='test-release'):
+        cmd = ['helm', 'template', release_name, self.chart_path]
+        for vf in values_files or []:
+            cmd.extend(['-f', vf])
+        if set_values:
+            for key, val in set_values.items():
+                cmd.extend(['--set', f'{key}={val}'])
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        if result.returncode == 0:
+            return result.stdout
+        return result.stderr
