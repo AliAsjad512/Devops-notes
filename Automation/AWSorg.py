54b955a9a61b589ab4ccba1085ed235ebec62aea
Ali Abbas <aa0504503@gmail.com>
2026-05-07 19:10:44 -0400

    list accounts

--- Automation/AWSorg.py
+++ Automation/AWSorg.py
@@ -9,3 +9,9 @@ class AWSOrgReport:
         self.org = boto3.client('organizations')
         self.ce = boto3.client('ce', region_name=region)
         self.config = boto3.client('config', region_name=region)
+    def list_accounts(self):
+        accounts = []
+        paginator = self.org.get_paginator('list_accounts')
+        for page in paginator.paginate():
+            accounts.extend(page['Accounts'])
+        return accounts
