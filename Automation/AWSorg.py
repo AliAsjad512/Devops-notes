b1cd789614bc3dfca34057e34d8d03904defb876
Ali Abbas <aa0504503@gmail.com>
2026-05-07 19:11:22 -0400

    get account cost

--- Automation/AWSorg.py
+++ Automation/AWSorg.py
@@ -15,3 +15,17 @@ class AWSOrgReport:
         for page in paginator.paginate():
             accounts.extend(page['Accounts'])
         return accounts
+    def get_account_cost(self, account_id, days=30):
+        end = datetime.date.today()
+        start = end - datetime.timedelta(days=days)
+        try:
+            response = self.ce.get_cost_and_usage(
+                TimePeriod={'Start': start.strftime('%Y-%m-%d'), 'End': end.strftime('%Y-%m-%d')},
+                Granularity='DAILY',
+                Metrics=['UnblendedCost'],
+                Filter={'Dimensions': {'Key': 'LINKED_ACCOUNT', 'Values': [account_id]}}
+            )
+            total = sum(float(day['Total']['UnblendedCost']['Amount']) for day in response['ResultsByTime'])
+            return round(total, 2)
+        except:
+            return None
