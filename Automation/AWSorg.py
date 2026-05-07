9a40ad554219f2ff46ada6abf5c78ec5fec96f15
Ali Abbas <aa0504503@gmail.com>
2026-05-07 19:10:09 -0400

    AWS Org Report

--- Automation/AWSorg.py
+++ Automation/AWSorg.py
@@ -3,3 +3,9 @@ import argparse
 import datetime
 import json
 from collections import defaultdict
+
+class AWSOrgReport:
+    def __init__(self, region='us-east-1'):
+        self.org = boto3.client('organizations')
+        self.ce = boto3.client('ce', region_name=region)
+        self.config = boto3.client('config', region_name=region)
