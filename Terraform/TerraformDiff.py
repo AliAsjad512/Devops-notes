4954cd6908bcf08d0d0d0cfa5cec346ea5aa7a04
Ali Abbas <aa0504503@gmail.com>
2026-05-08 12:51:21 -0400

    extracted resources

--- Terraform/TerraformDiff.py
+++ Terraform/TerraformDiff.py
@@ -11,4 +11,16 @@ class TfStateDiff:
     def load_state(self):
         with open(self.state_path, 'r') as f:
             return json.load(f)
+    def extract_resources(self, state):
+        resources = {}
+        for resource in state.get('resources', []):
+            if resource.get('mode') == 'managed':
+                for instance in resource.get('instances', []):
+                    key = f"{resource['type']}.{resource['name']}"
+                    resources[key] = {
+                        'type': resource['type'],
+                        'name': resource['name'],
+                        'attributes': instance.get('attributes', {})
+                    }
+        return resources
 
