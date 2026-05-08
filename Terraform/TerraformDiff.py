6cbb213f386ce4bec4a3170cdfe123d28ae0602f
Ali Abbas <aa0504503@gmail.com>
2026-05-08 12:50:17 -0400

    Class TfstateDiff

--- Terraform/TerraformDiff.py
+++ Terraform/TerraformDiff.py
@@ -4,3 +4,7 @@ import argparse
 import requests
 from deepdiff import DeepDiff
 from pathlib import Path
+
+class TfStateDiff:
+    def __init__(self, state_path):
+        self.state_path = Path(state_path)
