19a74bdb7124cdb7ecb51a9be1f6781613a8016d
Ali Abbas <aa0504503@gmail.com>
2026-05-09 14:52:00 -0400

    get top slow queries

--- Database/PostgresSQlPerformace.py
+++ Database/PostgresSQlPerformace.py
@@ -6,3 +6,14 @@ from datetime import datetime
 class PGPerformance:
     def __init__(self, host, port, user, password, database):
         self.conn = psycopg2.connect(host=host, port=port, user=user, password=password, database=database)
+
+   def get_top_slow_queries(self, limit=10):
+        cur = self.conn.cursor()
+        cur.execute("""
+            SELECT query, mean_time, calls, total_time
+            FROM pg_stat_statements
+            ORDER BY mean_time DESC
+            LIMIT %s
+        """, (limit,))
+        rows = cur.fetchall()
+        return [{'query': r[0][:200], 'mean_time_ms': round(r[1], 2), 'calls': r[2], 'total_time_sec': round(r[3] / 1000, 2)} for r in rows]
