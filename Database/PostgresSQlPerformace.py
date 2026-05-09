ddf430ba5687fcf53e84cb65742b8ab4d24abd2a
Ali Abbas <aa0504503@gmail.com>
2026-05-09 14:52:41 -0400

    Get Cache hit ratio

--- Database/PostgresSQlPerformace.py
+++ Database/PostgresSQlPerformace.py
@@ -17,3 +17,11 @@ class PGPerformance:
         """, (limit,))
         rows = cur.fetchall()
         return [{'query': r[0][:200], 'mean_time_ms': round(r[1], 2), 'calls': r[2], 'total_time_sec': round(r[3] / 1000, 2)} for r in rows]
+   def get_cache_hit_ratio(self):
+        cur = self.conn.cursor()
+        cur.execute("""
+            SELECT
+                sum(heap_blks_hit) / (sum(heap_blks_hit) + sum(heap_blks_read)) * 100 as hit_ratio
+            FROM pg_statio_user_tables
+        """)
+        return cur.fetchone()[0]
