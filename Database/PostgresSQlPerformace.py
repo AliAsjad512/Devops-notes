9e6bf5b61825d2b6c3e42b6d41c43ae81b51d8bc
Ali Abbas <aa0504503@gmail.com>
2026-05-09 14:51:18 -0400

    Class PG Performance

--- Database/PostgresSQlPerformace.py
+++ Database/PostgresSQlPerformace.py
@@ -2,3 +2,7 @@ import psycopg2
 import argparse
 import json
 from datetime import datetime
+
+class PGPerformance:
+    def __init__(self, host, port, user, password, database):
+        self.conn = psycopg2.connect(host=host, port=port, user=user, password=password, database=database)
