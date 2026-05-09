5b2ee35df1575ca2e4df62ed952b481293390f34
Ali Abbas <aa0504503@gmail.com>
2026-05-09 14:53:30 -0400

    get blocking lock

--- Database/PostgresSQlPerformace.py
+++ Database/PostgresSQlPerformace.py
@@ -25,3 +25,29 @@ class PGPerformance:
             FROM pg_statio_user_tables
         """)
         return cur.fetchone()[0]
+
+    def get_blocking_locks(self):
+        cur = self.conn.cursor()
+        cur.execute("""
+            SELECT
+                blocked_locks.pid AS blocked_pid,
+                blocked_activity.query AS blocked_query,
+                blocking_locks.pid AS blocking_pid,
+                blocking_activity.query AS blocking_query
+            FROM pg_locks blocked_locks
+            JOIN pg_stat_activity blocked_activity ON blocked_activity.pid = blocked_locks.pid
+            JOIN pg_locks blocking_locks ON blocking_locks.locktype = blocked_locks.locktype
+                AND blocking_locks.database IS NOT DISTINCT FROM blocked_locks.database
+                AND blocking_locks.relation IS NOT DISTINCT FROM blocked_locks.relation
+                AND blocking_locks.page IS NOT DISTINCT FROM blocked_locks.page
+                AND blocking_locks.tuple IS NOT DISTINCT FROM blocked_locks.tuple
+                AND blocking_locks.virtualxid IS NOT DISTINCT FROM blocked_locks.virtualxid
+                AND blocking_locks.transactionid IS NOT DISTINCT FROM blocked_locks.transactionid
+                AND blocking_locks.classid IS NOT DISTINCT FROM blocked_locks.classid
+                AND blocking_locks.objid IS NOT DISTINCT FROM blocked_locks.objid
+                AND blocking_locks.objsubid IS NOT DISTINCT FROM blocked_locks.objsubid
+                AND blocking_locks.pid != blocked_locks.pid
+            JOIN pg_stat_activity blocking_activity ON blocking_activity.pid = blocking_locks.pid
+            WHERE NOT blocked_locks.granted
+        """)
+        return cur.fetchall()
