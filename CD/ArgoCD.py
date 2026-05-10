61ade162ec5de4ff6d802b276f30901a7b89ba9d
Ali Abbas <aa0504503@gmail.com>
2026-05-10 13:55:40 -0400

    list application

--- CD/ArgoCD.py
+++ CD/ArgoCD.py
@@ -9,3 +9,8 @@ from urllib.parse import urljoin
         self.server_url = server_url.rstrip('/')
         self.headers = {'Authorization': f'Bearer {token}'}
         self.verify = not insecure
+    def list_applications(self):
+        url = urljoin(self.server_url, '/api/v1/applications')
+        resp = requests.get(url, headers=self.headers, verify=self.verify)
+        resp.raise_for_status()
+        return resp.json().get('items', [])
