5bcf1ed3a0056b59fb2c446d3c188d3a7d90dd8b
Ali Abbas <aa0504503@gmail.com>
2026-05-10 13:55:08 -0400

    ArgoCD Monitor

--- CD/ArgoCD.py
+++ CD/ArgoCD.py
@@ -3,3 +3,9 @@ import requests
 import json
 import time
 from urllib.parse import urljoin
+
+  class ArgoCDMonitor:
+    def __init__(self, server_url, token, insecure=True):
+        self.server_url = server_url.rstrip('/')
+        self.headers = {'Authorization': f'Bearer {token}'}
+        self.verify = not insecure
