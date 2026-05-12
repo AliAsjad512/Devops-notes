4b0fac06aaee98aa0c8f1439eba4667df437dbb1
Ali Abbas <aa0504503@gmail.com>
2026-05-12 03:00:59 -0400

    Git lab pipeline Triggger class

--- CI/GitlabCi.py
+++ CI/GitlabCi.py
@@ -2,3 +2,8 @@ import argparse
 import requests
 import json
 import sys
+
+class GitLabPipelineTrigger:
+    def __init__(self, project_id, token, gitlab_url='https://gitlab.com'):
+        self.url = f"{gitlab_url.rstrip('/')}/api/v4/projects/{project_id}/trigger/pipeline"
+        self.token = token
