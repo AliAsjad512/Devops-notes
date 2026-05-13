e6246c10263763ea3beaa94e1ceb9a17e29a6190
Ali Abbas <aa0504503@gmail.com>
2026-05-13 09:00:42 -0400

    Hashi corp vault Secret manager completed

--- Automation/HasicorpValuteSecret.py
+++ Automation/HasicorpValuteSecret.py
@@ -30,4 +30,34 @@ class VaultSecretManager:
             return resp['data']['keys']
         except:
             return []
+        
+    if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Vault Secret Manager')
+    parser.add_argument('--url', required=True, help='Vault URL')
+    parser.add_argument('--token', help='Vault token')
+    parser.add_argument('--role-id', help='AppRole role_id')
+    parser.add_argument('--secret-id', help='AppRole secret_id')
+    parser.add_argument('--action', choices=['write', 'read', 'delete', 'list'], required=True)
+    parser.add_argument('--path', required=True, help='Secret path (e.g., myapp/db)')
+    parser.add_argument('--data', help='JSON data for write')
+    parser.add_argument('--mount', default='secret')
+    args = parser.parse_args()
+
+    mgr = VaultSecretManager(args.url, args.token, args.role_id, args.secret_id)
+    if args.action == 'write':
+        if not args.data:
+            print("❌ --data required for write")
+            sys.exit(1)
+        data = json.loads(args.data)
+        mgr.write_secret(args.path, data, args.mount)
+    elif args.action == 'read':
+        secret = mgr.read_secret(args.path, args.mount)
+        if secret:
+            print(json.dumps(secret, indent=2))
+    elif args.action == 'delete':
+        mgr.delete_secret(args.path, args.mount)
+    elif args.action == 'list':
+        keys = mgr.list_secrets(args.path, args.mount)
+        for key in keys:
+            print(key)
 
