49655fe4236862b517510cc0dc48154672f5b386
Ali Abbas <aa0504503@gmail.com>
2026-05-13 08:57:00 -0400

    Vault Secret Manager class

--- Automation/HasicorpValuteSecret.py
+++ Automation/HasicorpValuteSecret.py
@@ -2,3 +2,12 @@ import argparse
 import hvac
 import json
 import sys
+
+class VaultSecretManager:
+    def __init__(self, url, token=None, role_id=None, secret_id=None):
+        self.client = hvac.Client(url=url, token=token)
+        if not self.client.is_authenticated():
+            if role_id and secret_id:
+                self.client.auth.approle.login(role_id=role_id, secret_id=secret_id)
+            else:
+                raise Exception("Authentication failed")
