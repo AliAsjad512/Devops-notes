c0e53875500d3b8196ac4b2fbf0105cbb901ed38
Ali Abbas <aa0504503@gmail.com>
2026-05-14 17:58:01 -0400

    list jobs

--- Automation/NomadJobStatusMonitor.py
+++ Automation/NomadJobStatusMonitor.py
@@ -5,3 +5,8 @@ import json
  class NomadMonitor:
     def __init__(self, addr='http://localhost:4646'):
         self.addr = addr.rstrip('/')
+
+    def list_jobs(self):
+        resp = requests.get(f"{self.addr}/v1/jobs")
+        resp.raise_for_status()
+        return resp.json()
