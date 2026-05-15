ec33cae376b5620ee38e09838fda7a6bf636eab0
Ali Abbas <aa0504503@gmail.com>
2026-05-15 17:44:46 -0400

    run cmd

--- Automation/Pulumi.py
+++ Automation/Pulumi.py
@@ -7,3 +7,8 @@ from pathlib import Path
     def __init__(self, stack_name, work_dir='.'):
         self.stack_name = stack_name
         self.work_dir = Path(work_dir)
+
+    def run_cmd(self, cmd):
+        full_cmd = ['pulumi', '--cwd', str(self.work_dir)] + cmd
+        result = subprocess.run(full_cmd, capture_output=True, text=True)
+        return result.returncode, result.stdout, result.stderr
