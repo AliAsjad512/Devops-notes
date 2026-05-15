924ff49f07528890637d18dab4dadffcb70fbb55
Ali Abbas <aa0504503@gmail.com>
2026-05-15 17:43:53 -0400

    Pulumi Manager

--- Automation/Pulumi.py
+++ Automation/Pulumi.py
@@ -3,3 +3,7 @@ import argparse
 import sys
 import json
 from pathlib import Path
+ class PulumiManager:
+    def __init__(self, stack_name, work_dir='.'):
+        self.stack_name = stack_name
+        self.work_dir = Path(work_dir)
