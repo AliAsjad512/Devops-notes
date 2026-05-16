c10b802b678449749b1a65a2e4a76e2cd9be07fd
Ali Abbas <aa0504503@gmail.com>
2026-05-16 01:24:19 -0400

    configRule Evaluator

--- AWS/AWSConfig.py
+++ AWS/AWSConfig.py
@@ -1,3 +1,6 @@
 import boto3
 import argparse
 import json
+ class ConfigRuleEvaluator:
+    def __init__(self, region='us-east-1'):
+        self.config = boto3.client('config', region_name=region)
