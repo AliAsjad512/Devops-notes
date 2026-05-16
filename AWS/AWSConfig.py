727bee3fc78c4c14f748a281cc4fd6fea84f2f97
Ali Abbas <aa0504503@gmail.com>
2026-05-16 01:25:52 -0400

    get non compliant resouces

--- AWS/AWSConfig.py
+++ AWS/AWSConfig.py
@@ -14,3 +14,17 @@ import json
                 'compliance': rule['Compliance']['ComplianceType']
             })
         return summary
+    
+     def get_non_compliant_resources(self, rule_name):
+        response = self.config.get_compliance_details_by_config_rule(
+            ConfigRuleName=rule_name,
+            ComplianceTypes=['NON_COMPLIANT']
+        )
+        resources = []
+        for evaluation in response.get('EvaluationResults', []):
+            resources.append({
+                'resource_id': evaluation['EvaluationResultIdentifier']['EvaluationResultQualifier']['ResourceId'],
+                'resource_type': evaluation['EvaluationResultIdentifier']['EvaluationResultQualifier']['ResourceType'],
+                'annotation': evaluation.get('Annotation', '')
+            })
+        return resources
