31db10d7f7bc044c5de11662752b28e5c8884eab
Ali Abbas <aa0504503@gmail.com>
2026-05-16 01:24:58 -0400

    get complince summary

--- AWS/AWSConfig.py
+++ AWS/AWSConfig.py
@@ -4,3 +4,13 @@ import json
  class ConfigRuleEvaluator:
     def __init__(self, region='us-east-1'):
         self.config = boto3.client('config', region_name=region)
+
+    def get_compliance_summary(self):
+        response = self.config.describe_compliance_by_config_rule()
+        summary = []
+        for rule in response['ComplianceByConfigRules']:
+            summary.append({
+                'rule_name': rule['ConfigRuleName'],
+                'compliance': rule['Compliance']['ComplianceType']
+            })
+        return summary
