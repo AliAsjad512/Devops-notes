5d1d8549c2f2660b23c17a606b2dc779006891ad
Ali Abbas <aa0504503@gmail.com>
2026-05-18 00:22:02 -0400

    Class New Relice Alert manager

--- Automation/NewReliceArtPolicymanager.py
+++ Automation/NewReliceArtPolicymanager.py
@@ -1,3 +1,9 @@
 import argparse
 import requests
 import json
+
+class NewRelicAlertManager:
+    def __init__(self, api_key, account_id, region='us'):
+        self.api_key = api_key
+        self.account_id = account_id
+        self.base_url = f'https://{region}.api.newrelic.com/graphql'
