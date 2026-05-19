50b3ed50875f1e3fcb2a280b0c43e1edf556b9ee
Ali Abbas <aa0504503@gmail.com>
2026-05-19 11:35:30 -0400

    Sys log server

--- logs/sysylog.py
+++ logs/sysylog.py
@@ -21,3 +21,7 @@ class SyslogHandler(socketserver.BaseRequestHandler):
         with open('syslog.json', 'a') as f:
             f.write(json.dumps(log_entry) + '\n')
         print(f"Received: {log_entry['host']} - {log_entry['message'][:100]}")
+class SyslogServer:
+    def __init__(self, host='0.0.0.0', port=514):
+        self.host = host
+        self.port = port
