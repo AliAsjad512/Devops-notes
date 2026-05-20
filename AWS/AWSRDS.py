cedeca21086d002755fdff00d450e81a7adfb281
Ali Abbas <aa0504503@gmail.com>
2026-05-20 07:28:26 -0400

    RDS Fail lover tester class

--- AWS/AWSRDS.py
+++ AWS/AWSRDS.py
@@ -2,3 +2,7 @@ import boto3
 import argparse
 import time
 import datetime
+
+class RDSFailoverTester:
+    def __init__(self, region='us-east-1'):
+        self.rds = boto3.client('rds', region_name=region)
