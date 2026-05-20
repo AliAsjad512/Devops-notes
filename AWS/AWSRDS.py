2d55d6bb4ed1e0f345d465928eaae296ca96cffe
Ali Abbas <aa0504503@gmail.com>
2026-05-20 07:29:41 -0400

    get instance status

--- AWS/AWSRDS.py
+++ AWS/AWSRDS.py
@@ -6,3 +6,11 @@ import datetime
 class RDSFailoverTester:
     def __init__(self, region='us-east-1'):
         self.rds = boto3.client('rds', region_name=region)
+    def get_instance_status(self, db_instance_id):
+        response = self.rds.describe_db_instances(DBInstanceIdentifier=db_instance_id)
+        instance = response['DBInstances'][0]
+        return {
+            'status': instance['DBInstanceStatus'],
+            'endpoint': instance.get('Endpoint', {}).get('Address'),
+            'multi_az': instance.get('MultiAZ', False)
+        }
