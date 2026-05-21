ef26205471201e412f4d5cc826d79ec1bf2dec10
Ali Abbas <aa0504503@gmail.com>
2026-05-21 11:22:36 -0400

    run cmd

--- Terraform/TerraformWorkspaceSwitcher.py
+++ Terraform/TerraformWorkspaceSwitcher.py
@@ -5,3 +5,6 @@ import re
 class TerraformWorkspace:
     def __init__(self, tf_dir='.'):
         self.tf_dir = tf_dir
+    def run_cmd(self, cmd):
+        result = subprocess.run(cmd, cwd=self.tf_dir, capture_output=True, text=True)
+        return result.returncode, result.stdout, result.stderr
