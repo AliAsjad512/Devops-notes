32295c8f90f2b77aac477adc4c80169807aead95
Ali Abbas <aa0504503@gmail.com>
2026-05-22 09:44:59 -0400

    list topics

--- Automation/kafkaTopicManager.py
+++ Automation/kafkaTopicManager.py
@@ -5,3 +5,7 @@ from kafka.errors import TopicAlreadyExistsError, UnknownTopicOrPartitionError
 class KafkaTopicManager:
     def __init__(self, bootstrap_servers='localhost:9092'):
         self.admin = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
+
+    def list_topics(self):
+        topics = self.admin.list_topics()
+        return topics
