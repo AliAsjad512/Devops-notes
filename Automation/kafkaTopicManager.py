3ac139d27df01bb2f4a50666c5e640c7e4279564
Ali Abbas <aa0504503@gmail.com>
2026-05-22 09:44:26 -0400

    Kafka Topic Manager class

--- Automation/kafkaTopicManager.py
+++ Automation/kafkaTopicManager.py
@@ -1,3 +1,7 @@
 import argparse
 from kafka.admin import KafkaAdminClient, NewTopic
 from kafka.errors import TopicAlreadyExistsError, UnknownTopicOrPartitionError
+
+class KafkaTopicManager:
+    def __init__(self, bootstrap_servers='localhost:9092'):
+        self.admin = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
