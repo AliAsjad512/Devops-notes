121521a1975bae63367967bb45f7ca62ea71be9d
Ali Abbas <aa0504503@gmail.com>
2026-05-22 09:45:56 -0400

    create topic

--- Automation/kafkaTopicManager.py
+++ Automation/kafkaTopicManager.py
@@ -9,3 +9,10 @@ class KafkaTopicManager:
     def list_topics(self):
         topics = self.admin.list_topics()
         return topics
+    def create_topic(self, name, partitions=1, replication_factor=1):
+        topic = NewTopic(name=name, num_partitions=partitions, replication_factor=replication_factor)
+        try:
+            self.admin.create_topics([topic])
+            print(f"✅ Topic '{name}' created")
+        except TopicAlreadyExistsError:
+            print(f"⚠️ Topic '{name}' already exists")
