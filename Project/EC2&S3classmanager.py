3e4f91559b83e6d9cf1e08605f01cca3ffa8b62b
Ali Abbas <aa0504503@gmail.com>
2026-05-23 10:55:22 -0400

    AWS class manager

--- Project/EC2&S3classmanager.py
+++ Project/EC2&S3classmanager.py
@@ -1,3 +1,9 @@
 import boto3
 import argparse
 import sys
+
+class AWSManager:
+    def __init__(self, region='us-east-1'):
+        self.ec2 = boto3.client('ec2', region_name=region)
+        self.s3 = boto3.client('s3')
+        self.region = region
