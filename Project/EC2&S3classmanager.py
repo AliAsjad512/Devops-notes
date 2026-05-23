af65f711c746a13018618fbcfccff1e0dd97d1db
Ali Abbas <aa0504503@gmail.com>
2026-05-23 11:00:15 -0400

    complete

--- Project/EC2&S3classmanager.py
+++ Project/EC2&S3classmanager.py
@@ -62,3 +62,56 @@ class AWSManager:
             self.s3.delete_object(Bucket=bucket_name, Key=obj['Key'])
         self.s3.delete_bucket(Bucket=bucket_name)
         print(f"Bucket {bucket_name} deleted")
+
+    def main():
+    parser = argparse.ArgumentParser(description='AWS EC2 & S3 Manager')
+    parser.add_argument('--region', default='us-east-1')
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    # EC2 commands
+    ec2_parser = subparsers.add_parser('ec2-list')
+    ec2_start = subparsers.add_parser('ec2-start')
+    ec2_start.add_argument('instance_id')
+    ec2_stop = subparsers.add_parser('ec2-stop')
+    ec2_stop.add_argument('instance_id')
+    ec2_tag = subparsers.add_parser('ec2-tag')
+    ec2_tag.add_argument('instance_id')
+    ec2_tag.add_argument('key')
+    ec2_tag.add_argument('value')
+    ec2_launch = subparsers.add_parser('ec2-launch')
+    ec2_launch.add_argument('--ami', required=True)
+    ec2_launch.add_argument('--type', default='t2.micro')
+    ec2_launch.add_argument('--key', help='Key pair name')
+    ec2_launch.add_argument('--sg', nargs='+', help='Security group IDs')
+
+    # S3 commands
+    s3_list = subparsers.add_parser('s3-list')
+    s3_create = subparsers.add_parser('s3-create')
+    s3_create.add_argument('bucket')
+    s3_delete = subparsers.add_parser('s3-delete')
+    s3_delete.add_argument('bucket')
+
+    args = parser.parse_args()
+    aws = AWSManager(args.region)
+
+    if args.command == 'ec2-list':
+        for inst in aws.list_instances():
+            print(f"{inst['Id']} [{inst['State']}] {inst['Name']} ({inst['Type']})")
+    elif args.command == 'ec2-start':
+        aws.start_instance(args.instance_id)
+    elif args.command == 'ec2-stop':
+        aws.stop_instance(args.instance_id)
+    elif args.command == 'ec2-tag':
+        aws.tag_instance(args.instance_id, args.key, args.value)
+    elif args.command == 'ec2-launch':
+        aws.launch_instance(args.ami, args.type, args.key, args.sg)
+    elif args.command == 's3-list':
+        for b in aws.list_buckets():
+            print(b)
+    elif args.command == 's3-create':
+        aws.create_bucket(args.bucket)
+    elif args.command == 's3-delete':
+        aws.delete_bucket(args.bucket)
+
+if __name__ == '__main__':
+    main()
