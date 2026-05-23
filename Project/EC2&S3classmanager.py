201f3c37dc7a82190c0607fbdcf6dc0bad542e72
Ali Abbas <aa0504503@gmail.com>
2026-05-23 10:56:27 -0400

    list instances

--- Project/EC2&S3classmanager.py
+++ Project/EC2&S3classmanager.py
@@ -7,3 +7,15 @@ class AWSManager:
         self.ec2 = boto3.client('ec2', region_name=region)
         self.s3 = boto3.client('s3')
         self.region = region
+    def list_instances(self):
+        resp = self.ec2.describe_instances()
+        instances = []
+        for reservation in resp['Reservations']:
+            for instance in reservation['Instances']:
+                instances.append({
+                    'Id': instance['InstanceId'],
+                    'Type': instance['InstanceType'],
+                    'State': instance['State']['Name'],
+                    'Name': next((tag['Value'] for tag in instance.get('Tags', []) if tag['Key'] == 'Name'), '')
+                })
+        return instances
