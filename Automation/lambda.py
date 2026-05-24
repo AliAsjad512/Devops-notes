428240a4d0a2e08d07c17f7dee0723ca5dc58c76
Ali Abbas <aa0504503@gmail.com>
2026-05-24 11:27:37 -0400

    S3 Fargate Pipeline

--- Automation/lambda.py
+++ Automation/lambda.py
@@ -2,3 +2,7 @@ import boto3
 import argparse
 import uuid
 import time
+class S3FargatePipeline:
+    def __init__(self, region='us-east-1'):
+        self.s3 = boto3.client('s3', region_name=region)
+        self.ecs = boto3.client('ecs', region_name=region)
