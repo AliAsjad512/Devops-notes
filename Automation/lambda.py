4f3f725a34d7e1078dd20b7f7add1ec272aff30f
Ali Abbas <aa0504503@gmail.com>
2026-05-24 11:28:14 -0400

    upload files

--- Automation/lambda.py
+++ Automation/lambda.py
@@ -6,3 +6,6 @@ class S3FargatePipeline:
     def __init__(self, region='us-east-1'):
         self.s3 = boto3.client('s3', region_name=region)
         self.ecs = boto3.client('ecs', region_name=region)
+    def upload_file(self, bucket, key, local_path):
+        self.s3.upload_file(local_path, bucket, key)
+        print(f"Uploaded {local_path} to s3://{bucket}/{key}")
