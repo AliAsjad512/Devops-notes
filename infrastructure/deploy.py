c49095d3a62ade0588ba5533fe0030001fe143d4
Ali Abbas <aa0504503@gmail.com>
2026-05-30 12:41:54 -0400

    create word counter stack

--- infrastructure/deploy.py
+++ infrastructure/deploy.py
@@ -3,3 +3,8 @@ import zipfile
 import os
 import json
 import time
+def create_word_counter_stack(bucket_name, function_name, topic_name):
+    iam = boto3.client('iam')
+    lambda_client = boto3.client('lambda')
+    s3 = boto3.client('s3')
+    sns = boto3.client('sns')
