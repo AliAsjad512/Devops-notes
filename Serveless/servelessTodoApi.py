9f58226d2dd0e7183737f5e6f8ded4cf4baaf3d8
Ali Abbas <aa0504503@gmail.com>
2026-06-01 10:32:20 -0400

    database libraries

--- Serveless/servelessTodoApi.py
+++ Serveless/servelessTodoApi.py
@@ -3,3 +3,6 @@ import boto3
 import os
 from uuid import uuid4
 from datetime import datetime
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ['TABLE_NAME']
+table = dynamodb.Table(table_name)
