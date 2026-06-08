0e3bfe89670a40e4114b8706c5a9f3538061ad0d
Ali Abbas <aa0504503@gmail.com>
2026-06-08 12:34:06 -0400

    list snapshots

--- AWS/RDSSnapShot.py
+++ AWS/RDSSnapShot.py
@@ -9,3 +9,10 @@ class RDSBackupCopier:
         self.target = boto3.client('rds', region_name=target_region)
         self.source_region = source_region
         self.target_region = target_region
+    def list_snapshots(self, days_back=7):
+        """List manual snapshots created in the last N days."""
+        import datetime
+        cutoff = datetime.datetime.now() - datetime.timedelta(days=days_back)
+        snapshots = self.source.describe_db_snapshots(SnapshotType='manual')['DBSnapshots']
+        recent = [s for s in snapshots if s['SnapshotCreateTime'].replace(tzinfo=None) > cutoff]
+        return recent
