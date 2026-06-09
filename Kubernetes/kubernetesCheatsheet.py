31b335f51b727cd8ab11d80ccf74acb9d3f33ab4
Ali Abbas <aa0504503@gmail.com>
2026-06-09 18:40:38 -0400

    first 10 kubernetes

---
+++ Kubernetes/kubernetesCheatsheet.py
@@ -0,0 +1,13 @@
+50 Most Frequently Used kubectl commands for DevOps Engineers [Part A]
+Kubectl is a command-line tool that allows users to interact with Kubernetes clusters. Kubectl is used to deploy applications, manage & monitor cluster resources.
+
+kubectl help ==> to get help and more information about any command.
+kubectl cluster-info ==> shares the K8s cluster information.
+kubectl cluster-info dump ==> for troubleshooting K8s cluster issues, we'll get logs.
+kubectl get nodes ==> lists all nodes of the cluster.
+kubectl describe node <node_name> ==> get all the details about a specific node, change the node name.
+kubectl version ==> the version of kubectl client, server and kustomize.
+kubectl get all ==> overview of all resources on the cluster like pods, deployments or services etc.
+kubectl options ==> Prints the kubectl command options that can be used with kubectl command.
+kubectl api-resources ==> Prints all the supported API resources on the server.
+kubectl api-versions ==> Prints all the supported API versions on the server.
