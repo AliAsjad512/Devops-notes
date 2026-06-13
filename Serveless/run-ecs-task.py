6232c73940fe18e9a86def829028b85cbf83c33d
Ali Abbas <aa0504503@gmail.com>
2026-06-13 19:50:24 -0400

    ECS Fargate Runner

--- Serveless/run-ecs-task.py
+++ Serveless/run-ecs-task.py
@@ -2,3 +2,10 @@ import boto3
 import argparse
 import json
 import time
+
+class ECSFargateRunner:
+    def __init__(self, cluster, task_definition, region='us-east-1'):
+        self.ecs = boto3.client('ecs', region_name=region)
+        self.cluster = cluster
+        self.task_definition = task_definition
+
