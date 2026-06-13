be5ddfd8f8d53b4797a2117d087b47c96a695e44
Ali Abbas <aa0504503@gmail.com>
2026-06-13 19:50:57 -0400

    run

--- Serveless/run-ecs-task.py
+++ Serveless/run-ecs-task.py
@@ -9,3 +9,29 @@ class ECSFargateRunner:
         self.cluster = cluster
         self.task_definition = task_definition
 
+
+        def run(self, subnets, security_groups, overrides=None, wait=False):
+        """Run a Fargate task and optionally wait for completion."""
+        response = self.ecs.run_task(
+            cluster=self.cluster,
+            taskDefinition=self.task_definition,
+            launchType='FARGATE',
+            networkConfiguration={
+                'awsvpcConfiguration': {
+                    'subnets': subnets,
+                    'securityGroups': security_groups,
+                    'assignPublicIp': 'ENABLED'
+                }
+            },
+            overrides=overrides or {}
+        )
+        if not response['tasks']:
+            print(f"Failed: {response['failures']}")
+            return None
+        task_arn = response['tasks'][0]['taskArn']
+        print(f"Started task: {task_arn}")
+        if wait:
+            self._wait(task_arn)
+        return task_arn
+
+
