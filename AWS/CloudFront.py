1baa891590ecea1eb3f9948cc5131e9ff2cb2f22
Ali Abbas <aa0504503@gmail.com>
2026-06-14 12:08:13 -0400

    invalidate

--- AWS/CloudFront.py
+++ AWS/CloudFront.py
@@ -6,3 +6,17 @@ class CloudFrontInvalidator:
     def __init__(self, distribution_id):
         self.cf = boto3.client('cloudfront')
         self.dist_id = distribution_id
+    def invalidate(self, paths=['/*'], wait=False):
+        """Invalidate paths (default: everything)."""
+        response = self.cf.create_invalidation(
+            DistributionId=self.dist_id,
+            InvalidationBatch={
+                'Paths': {'Quantity': len(paths), 'Items': paths},
+                'CallerReference': str(int(time.time()))
+            }
+        )
+        invalidation_id = response['Invalidation']['Id']
+        print(f"Invalidation {invalidation_id} started")
+        if wait:
+            self._wait(invalidation_id)
+        return invalidation_id
