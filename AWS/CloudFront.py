9860924c895ab9d69affd8296caf141a16350644
Ali Abbas <aa0504503@gmail.com>
2026-06-14 12:07:41 -0400

    cloud Front Invalidator

--- AWS/CloudFront.py
+++ AWS/CloudFront.py
@@ -1,3 +1,8 @@
 import boto3
 import argparse
 import time
+
+class CloudFrontInvalidator:
+    def __init__(self, distribution_id):
+        self.cf = boto3.client('cloudfront')
+        self.dist_id = distribution_id
