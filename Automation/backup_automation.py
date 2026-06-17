2efefe4600580bd601e5208fc2aefa04bdf8f057
Ali Abbas <aa0504503@gmail.com>
2026-06-16 20:06:20 -0400

    class Backup Manager

--- Automation/backup_automation.py
+++ Automation/backup_automation.py
@@ -2,3 +2,6 @@ import boto3
 import argparse
 import datetime
 import time
+class BackupManager:
+    def __init__(self, region='us-east-1'):
+        self.ec2 = boto3.client('ec2', region_name=region)
