e36aead734ec9ec91ab16d35d13a05ea8a5f48e1
Ali Abbas <aa0504503@gmail.com>
2026-06-17 18:56:04 -0400

    lambda handler

--- Automation/Ci/dlq_processor.py
+++ Automation/Ci/dlq_processor.py
@@ -6,3 +6,7 @@ sqs = boto3.client('sqs')
 sns = boto3.client('sns')
 dlq_url = os.environ['DLQ_URL']
 sns_topic = os.environ.get('SNS_TOPIC_ARN', '')
+
+def lambda_handler(event, context):
+    messages = event['Records']
+    failures = []
