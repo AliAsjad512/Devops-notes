e077afc68353c1cb7868e49d4ae177dec86fa018
Ali Abbas <aa0504503@gmail.com>
2026-06-22 14:37:57 -0400

    get all rules

--- Automation/config_collector.py
+++ Automation/config_collector.py
@@ -5,3 +5,7 @@ import json
 class ConfigCollector:
     def __init__(self, region='us-east-1'):
         self.config = boto3.client('config', region_name=region)
+    
+    def get_all_rules(self):
+        rules = self.config.describe_config_rules()['ConfigRules']
+        return rules
