54cca7b2121ea9f89179dd7a4d5a8a2d8dccae6e
Ali Abbas <aa0504503@gmail.com>
2026-06-23 15:37:38 -0400

    AB Health checker

--- AWS/alb_health_check.py
+++ AWS/alb_health_check.py
@@ -1,2 +1,7 @@
 import boto3
 import argparse
+
+class ALBHealthChecker:
+    def __init__(self, target_group_arn, region='us-east-1'):
+        self.elbv2 = boto3.client('elbv2', region_name=region)
+        self.tg_arn = target_group_arn
