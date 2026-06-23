762dc1b05b250f79f578db39703ef9b893c2eded
Ali Abbas <aa0504503@gmail.com>
2026-06-23 15:38:15 -0400

    check health

--- AWS/alb_health_check.py
+++ AWS/alb_health_check.py
@@ -5,3 +5,10 @@ class ALBHealthChecker:
     def __init__(self, target_group_arn, region='us-east-1'):
         self.elbv2 = boto3.client('elbv2', region_name=region)
         self.tg_arn = target_group_arn
+
+    def check_health(self):
+        """Get health status of all targets."""
+        response = self.elbv2.describe_target_health(TargetGroupArn=self.tg_arn)
+        targets = response['TargetHealthDescriptions']
+        healthy = 0
+        unhealthy = 0
