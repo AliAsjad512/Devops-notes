eab821596937ed2ba2f7a68a0fc1b59ac2af38a7
Ali Abbas <aa0504503@gmail.com>
2026-07-05 14:16:54 -0400

    create payload

--- AWS/budget_alert.py
+++ AWS/budget_alert.py
@@ -7,3 +7,12 @@ class BudgetManager:
         self.budget = boto3.client('budgets')
         self.account_id = account_id
      def create_budget(self, budget_name, limit_amount, limit_unit='USD', time_unit='MONTHLY', email=None):
+          payload = {
+            'AccountId': self.account_id,
+            'Budget': {
+                'BudgetName': budget_name,
+                'BudgetLimit': {'Amount': str(limit_amount), 'Unit': limit_unit},
+                'TimeUnit': time_unit,
+                'BudgetType': 'COST',
+                'CostFilters': {'Service': ['AmazonS3']}  # optional filter
+            },
