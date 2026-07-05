8fd2ae835b893cb48fd99eea914014d109f358f6
Ali Abbas <aa0504503@gmail.com>
2026-07-04 23:31:46 -0400

    added plain english

--- Kubernetes/learning/kubernetcConcepts.py
+++ Kubernetes/learning/kubernetcConcepts.py
@@ -29,3 +29,17 @@ If a pod dies, Kubernetes restarts it
 
 
 Control Plane NodeWorker NodeRuns pods?❌✅Runs API Server?✅❌Also calledMaster NodeJust "Node"In MinikubeSame machineSame machine
+
+
+Let's delete these files and start fresh. It will free up disk space and help avoid any performance issues while we work 
+
+
+kubectl to create, manage, inspect, and delete Kubernetes resources such as Pods, Deployments, Services, ConfigMaps, and Secrets. 
+
+The main purpose of creating a test Pod is to create a file inside it and store some data. Then, we delete the Pod to check whether the data is preserved or lost. This helps us understand whether the Pod's storage is persistent or ephemeral. 
+
+
+
+question.
+Your question in natural English
+"We're using this command to create a file inside a Pod, right? But the file is being created in the /tmp directory. How do I know that this file is actually being created inside the Pod and not somewhere else? What tells me that /tmp/myfile.txt belongs to the Pod? How can I verify that the file is inside the Pod?"
