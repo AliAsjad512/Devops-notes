00c0a564048af74aacb73d6d76ab8065b8d21cf4
Ali Abbas <aa0504503@gmail.com>
2026-07-05 14:15:19 -0400

    Budget class Manager

--- AWS/budget_alert.py
+++ AWS/budget_alert.py
@@ -1,3 +1,8 @@
 import boto3
 import argparse
 import json
+
+class BudgetManager:
+    def __init__(self, account_id, region='us-east-1'):
+        self.budget = boto3.client('budgets')
+        self.account_id = account_id
