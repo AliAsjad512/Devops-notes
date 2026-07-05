61eed6bb1820fbca7fd16115cd996ea3ea0e780a
Ali Abbas <aa0504503@gmail.com>
2026-07-05 14:16:10 -0400

    create budget

--- AWS/budget_alert.py
+++ AWS/budget_alert.py
@@ -6,3 +6,4 @@ class BudgetManager:
     def __init__(self, account_id, region='us-east-1'):
         self.budget = boto3.client('budgets')
         self.account_id = account_id
+     def create_budget(self, budget_name, limit_amount, limit_unit='USD', time_unit='MONTHLY', email=None):
