138d796e27ed88e619ae44daea3cdc413923427b
Ali Abbas <aa0504503@gmail.com>
2026-07-10 19:16:04 -0400

    CodeBuildManager class

--- Automation/codebuild_manager.py
+++ Automation/codebuild_manager.py
@@ -2,3 +2,7 @@ import boto3
 import argparse
 import time
 import sys
+
+class CodeBuildManager:
+    def __init__(self, region='us-east-1'):
+        self.codebuild = boto3.client('codebuild', region_name=region)
